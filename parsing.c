0fa97e46a4a37d292997511a405adaaf392dd44d
Ayomide Bamidele <ayoeze@hotmail.com>
2020-08-21 23:18:39 +0100

    basic calculator functionality

--- parsing.c
+++ parsing.c
@@ -13,14 +13,101 @@ char* readline(char* prompt) {
 }
 void add_history(char* unused){}
 #else 
-#include <stdio.h>
-#include <stdlib.h>
 #include <editline/readline.h>
 #include <editline/history.h>
 #endif
 
-int main(int argc, char const *argv[])
-{
+int number_of_nodes(mpc_ast_t* tree) {
+    if(tree->children_num == 0) { return 1; }
+    if(tree->children_num >= 1) {
+        int total = 1;
+        for (int i = 0; i < tree->children_num; i++)
+        {
+            total += number_of_nodes(tree->children[i]);
+        }
+        return total;
+    }
+    return 0;
+}
+
+int number_of_branches(mpc_ast_t* tree) {
+    if(tree->children_num == 0) { return 0; }
+    if(tree->children_num >= 1) {
+        int total = tree->children_num;
+        for (int i = 0; i < tree->children_num; i++)
+        {
+            total += number_of_branches(tree->children[i]);
+        }
+        return total;
+    }
+    return 0;
+}
+
+int eval_op(long x, char* op, long y){
+    if(strcmp(op, "+") == 0){ return x + y;}
+    if(strcmp(op, "-") == 0){ return x - y;}
+    if(strcmp(op, "*") == 0){ return x * y;}
+    if(strcmp(op, "/") == 0){ return x / y;}
+    if(strcmp(op, "%") == 0){ return x % y;}
+    return 0;
+}
+
+long eval(mpc_ast_t* tree) {
+
+    /* if tagged as number, return directly */
+
+    if(strstr(tree->tag, "number")){
+        return atoi(tree->contents);
+    }
+
+    /* the operator is always the second child */
+
+    char* op = tree->children[1]->contents;
+
+    /* store child  */
+
+    long x = eval(tree->children[2]);
+
+    /* Iterate through remaining children and combine */
+    int i = 3;
+    while (strstr(tree->children[i]->tag, "expr"))
+    {
+        x = eval_op(x, op, eval(tree->children[i]));
+        i++;
+    }
+    
+    return x;
+}
+
+void debug_tree(mpc_ast_t* a) {
+    printf("Tag: %s\n", a->tag);
+    printf("Contents: %s\n", a->contents);
+    printf("Number of children: %i\n", a->children_num);
+    mpc_ast_t* c0 = a->children[0];
+    printf("First Child Tag: %s\n", c0->tag);
+    printf("First Child Contents: %s\n", c0->contents);
+    printf("First Child Number of children: %i\n",
+    c0->children_num);
+    printf("number of tree children %i\n", number_of_nodes(a));
+    printf("number of branches: %i\n", number_of_branches(a));
+}
+
+
+int main(int argc, char const *argv[]) {
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Ayolisp  = mpc_new("ayolisp");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+(\\.[0-9]*)?/;                  \
+      operator : '+' | '-' | '*' | '/' | '%' ;            \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      ayolisp    : /^/ <operator> <expr>+ /$/ ;           \
+    ",
+    Number, Operator, Expr, Ayolisp);
+
     puts("\nAyolisp Version 0.0.0.0.1");
     puts("Press Ctrl+C to exit \nWhat will you lisp today? \n");
 
@@ -30,10 +117,21 @@ int main(int argc, char const *argv[])
 
         add_history(input);
 
-        printf("No you're a %s\n", input);
+        mpc_result_t r;
+
+        if(mpc_parse("<stdin>", input, Ayolisp, &r)){
+            long result = eval(r.output);
+            mpc_ast_t* a = r.output;
+            printf("%li\n", result);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
     }
     
+    mpc_cleanup(4, Number, Operator, Expr, Ayolisp);
     return 0;
 }
