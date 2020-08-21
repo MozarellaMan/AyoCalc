a86afdf9168650a079aff0c3ba68792a5436bee2
Ayomide Bamidele <ayoeze@hotmail.com>
2020-08-21 18:18:55 +0100

    first commit!

---
+++ hello_world.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+static char input[2048];
+
+int main(int argc, char const *argv[])
+{
+    puts("Ayolisp Version 0.0.0.0.1");
+    puts("Press Ctrl+c to exit \n");
+
+    while (1)
+    {
+        fputs("ayoliosp> ", stdout);
+
+        fgets(input, 2048, stdin);
+
+        printf("No you're a %s", input);
+    }
+    
+    return 0;
+}
